df68b5573d5261ccd9093f32567b7f6ca8fcea2e
ginobilinie <ginobilinie@gmail.com>
2023-07-10 19:22:01 -0400

    update training fash

--- tools/basic_fashion-2019-FGVC6/demo.sh
+++ tools/basic_fashion-2019-FGVC6/demo.sh
@@ -1,6 +1,22 @@
 export MODEL_NAME="runwayml/stable-diffusion-v1-5"
-export DATA_DIR="imaterialist-fashion-2019-FGVC6"
+export DATA_DIR="/data/rlan/data/imaterialist-fashion-2019-FGVC6"
+export RESUME_MODEL="latest"
+python train_fashion_2019.py \
+    --pretrained_model_name_or_path $MODEL_NAME \
+    --resolution 256 --random_flip \
+    --train_batch_size 1 --gradient_accumulation_steps 16 --gradient_checkpointing \
+    --max_train_steps 15000 \
+    --checkpointing_steps 1000 --checkpoints_total_limit 1 \
+    --learning_rate 1e-05 --max_grad_norm=1 --lr_warmup_steps=0 \
+    --conditioning_dropout_prob=0.05 \
+    --use_8bit_adam \
+    --enable_xformers_memory_efficient_attention \
+    --set_grads_to_none \
+    --seed=42 \
+    --train_data_dir $DATA_DIR \
+    --resume_from_checkpoint $RESUME_MODEL
 
+:'
 function demo_11GB(){
     python train_fashion_2019.py \
         --pretrained_model_name_or_path $MODEL_NAME \
@@ -57,3 +73,4 @@ function test(){
         --train_data_dir $DATA_DIR \
         --save_data_dir $save_data_dir
 }
+'
